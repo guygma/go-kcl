e6aa322c34216870e8a1f70fbf91264797cea233
Guy Marcus <gmarcus@ns1.com>
2019-08-20 14:48:19 -0400

    First step in restructuring to not be dependent on building into a Docker. Rather, just a plain reimplentation of the KCL in Go. Missing features (like de-aggregation) to be added.

--- worker/worker.go
+++ worker/worker.go
@@ -39,7 +39,7 @@ import (
 	"github.com/aws/aws-sdk-go/service/kinesis"
 	"github.com/aws/aws-sdk-go/service/kinesis/kinesisiface"
 
-	chk "github.com/vmware/vmware-go-kcl/clientlibrary/checkpoint"
+	chk "go-kcl/checkpoint"
 	"github.com/vmware/vmware-go-kcl/clientlibrary/config"
 	kcl "github.com/vmware/vmware-go-kcl/clientlibrary/interfaces"
 	"github.com/vmware/vmware-go-kcl/clientlibrary/metrics"
